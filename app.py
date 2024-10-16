bc74f3c3c1766c9efdcaa8ae6133fbdf1aba0d23
Anchit Mehra <119131473+AnMaster15@users.noreply.github.com>
2024-10-16 15:12:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -75,8 +75,11 @@ def download_single_audio(url, index, download_path):
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'cookiefile': 'cookies.txt',  # Path to cookies file
+    'retries': 10,
+    'proxy': 'socks5://127.0.0.1:9050',  # Use a proxy or Tor for anonymity
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',  # Spoof user agent
 }
+
 
 
     max_attempts = 5
